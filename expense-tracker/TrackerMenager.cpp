a968b2886d2ec0ee7dfbebf33a85df649833385e
Alex Yagni <near1tfeex@gmail.com>
2024-11-01 16:29:33 +0500

    Fixed logic bags

--- expense-tracker/TrackerMenager.cpp
+++ expense-tracker/TrackerMenager.cpp
@@ -79,21 +79,21 @@ CommendTypes ParseCommendTypes(std::string_view str) {
 
 	auto found{ std::ranges::find(names, str) };
 	if (found == names.end()) {
-		throw std::invalid_argument(std::format("Error:\tinvalide argument - {}!", str));
+		throw std::invalid_argument(std::format("Error:\tinvalide argument - \"{}\"!", str));
 	}
 
 	return static_cast<CommendTypes>(found - names.begin());
 }
 
 static void IsValideCountArgument(const std::vector<std::string>& commend, std::size_t count) {
-	if (commend.size() < count) {
+	if (commend.size() != count) {
 		throw std::invalid_argument("Error:\tMissing arguments for command!");
 	}
 }
 
 static std::string_view GetValideDescription(const std::vector<std::string>& commend, std::size_t index) {
 	if (commend[index] != "--description") {
-		throw std::invalid_argument(std::format("Error:\tinvalide argument - {}! {}",
+		throw std::invalid_argument(std::format("Error:\tinvalide argument - \"{}\"! {}",
 			commend[index], "You should use \"--description\"!"));
 	}
 
@@ -102,31 +102,54 @@ static std::string_view GetValideDescription(const std::vector<std::string>& com
 
 static float GetValideAmount(const std::vector<std::string>& commend, std::size_t index) {
 	if (commend[index] != "--amount") {
-		throw std::invalid_argument(std::format("Error:\tinvalide argument - {}! {}",
+		throw std::invalid_argument(std::format("Error:\tinvalide argument - \"{}\"! {}",
 			commend[index], "You should use \"--amount\"!"));
 	}
 
-	return static_cast<float>(std::stoi(commend[index + 1])); //add comm
+	float amount;
+	try {
+		amount = static_cast<float>(std::stoi(commend[index + 1])); //why index + 1? add comm OK/
+	}
+	catch (std::invalid_argument&) {
+		throw std::invalid_argument(std::format("You have entered \"{}\" and you needed a number!", commend[index + 1]));
+	}
+
+	return amount;
 }
 
 static ID GetValidID(const std::vector<std::string>& commend, std::size_t index) {
 	if (commend[index] != "--id") {
-		throw std::invalid_argument(std::format("Error:\tinvalide argument - {}! {}",
+		throw std::invalid_argument(std::format("Error:\tinvalide argument - \"{}\"! {}",
 			commend[index], "You should use \"--id\"!"));
 	}
 
-	return static_cast<ID>(std::stoi(commend[index + 1]));
+	ID id;
+	try {
+		id = static_cast<ID>(std::stoi(commend[index + 1])); //why index + 1? add comm OK/
+	}
+	catch (std::invalid_argument&) {
+		throw std::invalid_argument(std::format("You have entered \"{}\" and you needed a number!", commend[index + 1]));
+	}
+
+	return id;
 }
 
 static MonthTypes IsValidMonth(const std::vector<std::string>& commend, std::size_t index) {
 	if (commend[index] != "--month") {
-		throw std::invalid_argument(std::format("Error:\tinvalide argument - {}! {}",
+		throw std::invalid_argument(std::format("Error:\tinvalide argument - \"{}\"! {}",
 			commend[index], "You should use \"--month\"!"));
 	}
 
-	MonthTypes month{ static_cast<MonthTypes>(std::stoi(commend[index + 1]) - 1) };
+	MonthTypes month;
+	try {
+		month = static_cast<MonthTypes>(std::stoi(commend[index + 1]) - 1); //why index + 1? add comm OK/
+	}
+	catch (std::invalid_argument&) {
+		throw std::invalid_argument(std::format("You have entered \"{}\" and you needed a number!", commend[index + 1]));
+	}
+
 	if (MonthTypes::January > month || month > MonthTypes::December) {
-		throw std::invalid_argument(std::format("Error:\tinvalide argument - {}! {}",
+		throw std::invalid_argument(std::format("Error:\tinvalide argument - \"{}\"! {}",
 			static_cast<int>(month), "You should use \"1 - 12\"!"));
 	}
 
@@ -140,7 +163,7 @@ void HendlerCommendAdd(const std::vector<std::string>& commend, std::vector<Expe
 
 	ID id{ 1 };
 	if (!expenses.empty()) {
-		id = expenses.back().GetID();
+		id = expenses.back().GetID() + 1;
 	}
 
 	expenses.emplace_back(Expense{ id, description, amount });
@@ -152,7 +175,7 @@ static auto FindOrderByID(std::vector<Expense>& expenses, ID id) {
 		[id](const auto& item) -> bool { return item.GetID() == id; }) };
 	if (found == expenses.end()) {
 		throw std::runtime_error(
-			std::format("Error:\tThe object with id - {} was not found!", id));
+			std::format("Error:\tThe object with id - \"{}\" was not found!", id));
 	}
 
 	return found;
@@ -174,12 +197,12 @@ void HendlerCommendUpdate(const std::vector<std::string>& commend, std::vector<E
 }
 
 void HendlerCommendDelete(const std::vector<std::string>& commend, std::vector<Expense>& expenses) {
-	IsValideCountArgument(commend, 5);
+	IsValideCountArgument(commend, 3);
 	if (expenses.empty()) {
 		throw std::runtime_error("Error:\tEmpty list expenses!");
 	}
 
-	ID id{ GetValidID(commend, 3) };
+	ID id{ GetValidID(commend, 1) };
 	auto found{ FindOrderByID(expenses, id) };
 	
 	expenses.erase(found);
@@ -207,13 +230,14 @@ void HendlerCommendList(const std::vector<std::string>& commend, std::vector<Exp
 }
 
 void HendlerCommendSummary(const std::vector<std::string>& commend, std::vector<Expense>& expenses) {
-	if (expenses.size() == 1) {
+	if (commend.size() == 1) {
 		float sum{ std::accumulate(expenses.begin(), expenses.end(), 0.0F,
 			[](float acc, const auto& item) -> float { return acc += item.GetAmount(); }) };
 		std::cout << std::format("Total expenses: {}\n", sum);
 		return;
 	}
 
+	IsValideCountArgument(commend, 3);
 	MonthTypes type{ IsValidMonth(commend, 1) };
 	float sum{ std::accumulate(expenses.begin(), expenses.end(), 0.0F,
 			[type](float acc, const auto& item) -> float {
@@ -221,5 +245,5 @@ void HendlerCommendSummary(const std::vector<std::string>& commend, std::vector<
 				return month == static_cast<std::chrono::month>(static_cast<int>(type) + 1) ? 
 					acc + item.GetAmount() : acc; }) };
 
-	std::cout << std::format("Total expenses: {}\n", sum);
+	std::cout << std::format("Total expenses: {}.\tPer month {}\n", sum, static_cast<int>(type) + 1);
 }
